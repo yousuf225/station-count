10fe0c5fb1bbb06aad065b922855d74ca7b55f05
yousuf225 <116723483+yousuf225@users.noreply.github.com>
2023-01-28 00:19:05 +0530

    added files from upload

---
+++ first.js
@@ -0,0 +1,27 @@
+
+ let count = 0 
+
+function increment() {
+   count += 1 
+   document.getElementById("count-el").innerText = count
+  
+   
+}
+function save(){
+    
+    let p = count  + " - "
+    document.getElementById("save-el").textContent += p
+    console.log(count)
+    document.getElementById("count-el").innerText = 0
+    count = 0
+}
+function reset(){
+    document.getElementById("count-el").innerText = 0
+    count = 0
+}
+
+
+
+
+
+
