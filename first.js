5f3b51f028e13644c80df28d9fb6987b164d0c82
yousuf225 <116723483+yousuf225@users.noreply.github.com>
2023-05-14 23:37:19 +0530

    Update first.js

--- first.js
+++ first.js
@@ -12,8 +12,8 @@ function save(){
     let p = count  + " - "
     document.getElementById("save-el").textContent += p
     console.log(count)
-    document.getElementById("count-el").innerText = 0
-    count = 0
+    document.getElementById("count-el").innerText = ""
+    count = ""
 }
 function reset(){
     document.getElementById("count-el").innerText = ""
